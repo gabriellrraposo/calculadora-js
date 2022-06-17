cc923abf5de69f2fb95f30ec25778bbc42feae5a
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-17 10:35:12 -0300

    Valida se a opção digitada é válida

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function calculadora() {
-    const operacao = prompt("Selecione o tipo de operação que você deseja realizar\n1 - Soma [+]\n2 - Subtração [-]\n3 - Multiplicação [*]\n4 - Divisão [/]\n5 - Divisão Inteira [%]\n6 - Potenciação [**]")
-
+    let operacao = prompt("Selecione o tipo de operação que você deseja realizar\n1 - Soma [+]\n2 - Subtração [-]\n3 - Multiplicação [*]\n4 - Divisão [/]\n5 - Divisão Inteira [%]\n6 - Potenciação [**]")
+    operacao = Number(operacao)
+        while (operacao < 1 || operacao > 6) {
+            alert('Digite uma opção válida!')
+            let operacao = prompt("Selecione o tipo de operação que você deseja realizar\n1 - Soma [+]\n2 - Subtração [-]\n3 - Multiplicação [*]\n4 - Divisão [/]\n5 - Divisão Inteira [%]\n6 - Potenciação [**]")
+            operacao = Number(operacao)
+            if (operacao >= 1 || operacao <= 6) {
+                break
+            }
+        }
+        
     let n1 = prompt('Insira o primeiro número da operação:')
     let n2 = prompt('Insira o segundo número da operação:')
-    let resultado
 
     function soma() {
         n1 = Number(n1)
@@ -53,8 +61,6 @@ function calculadora() {
         divInteira()
     } else if (operacao == 6) {
         potenciacao()
-    } else {
-        alert('Digite uma opção válida!')
     }
 }
 
